049152438c4019fd2fc5c20bae6e1cdc9b90f928
TunchinKao <tunchinkao@gmail.com>
2022-08-07 13:07:09 +0800

    add vimrc

---
+++ VIM/vimrc.cpp
@@ -0,0 +1,17 @@
+syntax enable /off
+set number / nonumber
+set tabstop=4
+icolo default/darkblue/koehler/desrt/ron/torte
+nocompatible
+mouse = a
+ai
+cindent
+=tabprevious <CR>
+=tabnext<CR>
+sp
+vsp
+<C-w> {n} {< + - >?}
+tabnew
+nore -> norecursion
+nnoremap {lhs} {rhs}
+nnoremap <C-Left> <C-Right>
