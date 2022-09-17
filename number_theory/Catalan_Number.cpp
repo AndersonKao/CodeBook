9909cf91715eaa69be84423d2c50238efd8d994a
TunchinKao <tunchinkao@gmail.com>
2022-09-17 21:49:58 +0800

    renew catalan

--- number_theory/Catalan_Number.cpp
+++ number_theory/Catalan_Number.cpp
@@ -20,7 +20,7 @@ The number of rooted full binary trees with n+1 leaves (vertices are not numbere
 The number of ways to completely parenthesize n+1 factors.
 The number of triangulations of a convex polygon with n+2 sides (i.e. the number of partitions of polygon into disjoint triangles by using the diagonals).
 The number of ways to connect the 2n points on a circle to form n disjoint chords.
-The number of non-isomorphic full binary trees with n internal nodes (i.e. nodes having at least one son).
+The number of non-isomorphic full binary trees with n internal nodes (i.e. nodes having at least one son). *full binary tree: nodes with either 2 or no children.
 The number of monotonic lattice paths from point (0,0) to point (n,n) in a square lattice of size n*n, which do not pass above the main diagonal (i.e. connecting (0,0) to (n,n)).
 Number of permutations of length n that can be stack sorted (i.e. it can be shown that the rearrangement is stack sorted if and only if there is no such index i<j<k, such that a_k < a_i < a_j ).
 The number of non-crossing partitions of a set of n elements.
