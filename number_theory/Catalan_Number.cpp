f514756141ab51ab5b2e76cb718474c85719f45e
TunchinKao <tunchinkao@gmail.com>
2022-09-17 22:01:51 +0800

    added catalan or

--- number_theory/Catalan_Number.cpp
+++ number_theory/Catalan_Number.cpp
@@ -14,9 +14,14 @@ void init(int n) {
         }
     }
 }
+/*
+C[0] = C[1] = 1.
+C[n] = C[k]*C[n-1-k], k from 0 to n-1.  if n >= 2.
+*/
 /* solution for
 Number of correct bracket sequence consisting of n opening and n closing brackets.
 The number of rooted full binary trees with n+1 leaves (vertices are not numbered). A rooted binary tree is full if every vertex has either two children or no children.
+The number of binary search trees that will be formed with N keys.
 The number of ways to completely parenthesize n+1 factors.
 The number of triangulations of a convex polygon with n+2 sides (i.e. the number of partitions of polygon into disjoint triangles by using the diagonals).
 The number of ways to connect the 2n points on a circle to form n disjoint chords.
