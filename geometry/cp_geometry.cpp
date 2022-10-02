79a280874f5ee5c70961ba17fb08da0c698c4c7c
liuching <benjack30518@gmail.com>
2022-10-02 22:00:38 +0800

    Line finish (need check)

--- geometry/cp_geometry.cpp
+++ geometry/cp_geometry.cpp
@@ -1,187 +1,133 @@
 #include <bits/stdc++.h>
 using namespace std;
-const double eps = 1e-6;
-inline int fcmp(const double &a, const double &b){
-    if(fabs(a - b) < eps)
-        return 0;
-    return ((a - b > 0.0) * 2) - 1;
+
+const double eps = 1e-9;
+double fcmp(double a, double b = 0, double eps = 1e-9) {
+    if (abs(a-b) < eps) return 0;
+    return a-b;
 }
 
-template <typename T>
 struct Point
 {
-    T x, y;
+    double x, y;
     Point() : x(0), y(0) {}
-    Point(const T x, const T y) : x(x), y(y) {}
-    template <class F>
-    explicit operator Point<F>() const
-    {
-        return Point<F>((F)x, (F)y);
-    }
-
-    Point operator+(const Point b) const
-    {
+    Point(double x, double y) : x(x), y(y) {}
+    
+    Point operator+(Point b) {
         return Point(x + b.x, y + b.y);
     }
-    Point operator-(const Point b) const
-    {
+    
+    Point operator-(Point b) {
         return Point(x - b.x, y - b.y);
     }
-    template <class F>
-    Point<F> operator*(const F fac)
-    {
-        return Point<F>(x * fac, y * fac);
+    
+    Point operator*(double fac) {
+        return Point(x * fac, y * fac);
     }
-    template <class F>
-    Point<F> operator/(const F fac)
-    {
-        return Point<F>(x / fac, y / fac);
+    
+    Point operator/(double fac) {
+        return Point(x / fac, y / fac);
     }
 
-    T operator&(const Point b) const { return x * b.x + y * b.y; }
+    double operator&(Point b) { return x * b.x + y * b.y; }
     // dot operator
-    T operator^(const Point b) const { return x * b.y - y * b.x; }
+    double operator^(Point b) { return x * b.y - y * b.x; }
     // cross operator
-    bool operator==(const Point b) const
-    {
-        if(std::is_integral<T>::value)
-            return x == b.x and y == b.y;
+    bool operator==(Point b) const {
         return fcmp(x, b.x) == 0 && fcmp(y, b.y) == 0;
     }
-    bool operator<(const Point b) const
-    {
-        if(std::is_integral<T>::value)
-            return x == b.x ? y < b.y : x < b.x;
-        return fcmp(x, b.x) == 0 ? (fcmp(y, b.y)) < 0 : (fcmp(x, b.x) < 0);
-    } // 字 典 序
+    
+    bool operator<(Point b) const {
+        if (fcmp(x, b.x) == 0)
+            return y < b.y;
+        return x < b.x;
+    }
 
-    Point operator-() const { return Point(-x, -y); }
-    T norm() const { return *this & *this; }    // 歐 式 長 度 平 方
-    Point prep() const { return Point(-y, x); } // 左 旋 直 角 法 向 量
-    template <class F>
-    friend istream &operator>>(istream &is, Point<F> &pt);
-    template<class F>
-    friend ostream &operator<<(ostream &os, const Point<F> &pt);
+    double norm() { return *this & *this; }    // 歐 式 長 度 平 方
+    Point prep() { return Point(-y, x); } // 左 旋 直 角 法 向 量
 };
-template <class F>
-ostream &operator<<(ostream &os, const Point<F> &pt)
-{
-    return os << "(" << pt.x << " " << pt.y << ")";
-}
-template <class F>
-istream &operator>>(istream &is, Point<F> &pt)
-{
-    return is >> pt.x >> pt.y;
-}
-template <class F>
-bool collinearity(const Point<F>& p1, const Point<F>& p2, const Point<F>& p3){
-    if(std::is_integral<F>::value)
-        return (p1 - p3) ^ (p2 - p3) == 0;
-    return fcmp((p1 - p3) ^ (p2 - p3), 0.0) == 0;
-}
-// check co-line first. properly
-template<class F>
-inline bool btw(const Point<F>& p1, const Point<F>& p2, const Point<F>& p3){
-    if(std::is_integral<F>::value)
-        return ((p1 - p3) & (p2 - p3)) <= 0;
-    return fcmp((p1 - p3) & (p2 - p3), 0.0) <= 0;
+
+bool collinearity(Point p1, Point p2, Point p3){
+    return fcmp((p1 - p3) ^ (p2 - p3)) == 0;
 }
 
-// is p3 on (p1, p2)?
-template<class F>
-inline bool pointOnSegment(const Point<F>& p1, const Point<F>& p2, const Point<F>& p3){
+bool btw(Point p1, Point p2, Point p3){
+    return fcmp((p1 - p3) & (p2 - p3)) <= 0;
+}
+
+bool pointOnSegment(Point p1, Point p2, Point p3){
     return collinearity(p1, p2, p3) && btw(p1, p2, p3);
 }
 
-template <typename T, typename Real = double>
 struct Line
 {
-    Point<T> st, ed;
-    Point<T> vec() const { return ed - st; }
-    T ori(const Point<T> p) const { return (ed - st) ^ (p - st); }
-    int orint(const Point<T> p) const{
-        T a = this->ori(p);
-        return (fcmp(a, 0.0)); // 1 on posi-side // -1 nega-side
-        // a little bit useless?
-    }
-    Line(const Point<T> x, const Point<T> y) : st(x), ed(y) {}
-    template <class F>
-    operator Line<F>() const
-    {
-        return Line<F>((Point<F>)st, (Point<F>)ed);
-    }
+    Point sp, ep;
+    
+    Line() {}
+    Line(Point sp, Point ep) : sp(sp), ep(ep) {}
+    Line(double x1, double y1, double x2, double y2) : sp(Point(x1, y1)), ep(Point(x2, y2)) {}
+    
+    Point vec() { return ep - sp; }
 
-    // sort by arg, the left is smaller for parallel lines
-    bool operator<(Line B) const
-    {
-        Point<T> a = vec(), b = B.vec();
-        auto sgn = [](const Point<T> t) { return (t.y == 0 ? t.x : t.y) < 0; };
-        if (sgn(a) != sgn(b))
-            return sgn(a) < sgn(b);
-        if (abs(a ^ b) == 0)
-            return B.ori(st) > 0;
-        return (a ^ b) > 0;
-    }
-
-    // Regard a line as a function
-    template <typename F>
-    Point<F> operator()(const F x) const // A + AB * x = the point position.
-    {
-        return Point<F>(st) + vec() * x;
-    }
-
-    bool isSegProperIntersection(const Line l) const
-    {
-        return l.ori(st) * l.ori(ed) < 0 and ori(l.st) * ori(l.ed) < 0;
-    }
-    bool isSegIntersection(const Line l)const{
-        Line<Real> h = *this;
-        // hst = 1, hed = 2, lst = 3, led = 4
-        double hlst = h.ori(l.st);
-        double hled = h.ori(l.ed);
-        double lhst = l.ori(h.st);
-        double lhed = l.ori(h.ed);
-        if(fcmp(hlst, 0.0) == 0 && fcmp(hled,0.0) == 0)
-            return h.isPointOnSeg(l.st) || h.isPointOnSeg(l.ed) || l.isPointOnSeg(h.st) || l.isPointOnSeg(h.ed);
-
-        return fcmp(hlst * hled, 0.0) <= 0 && fcmp(lhst * lhed, 0.0) <= 0;
+    double ori(Point src) { 
+        return (ep - sp) ^ (src - sp);
     }
     
-    bool isPointOnSegProperly(const Point<T> p) const
+    //* from here
+    // Regard a line as a function
+    Point operator()(double x) // A + AB * x = the point position.
     {
-        return fcmp(ori(p), 0.0) == 0 and fcmp(((st - p) & (ed - p)), 0.0) < 0;
-    }
-    bool isPointOnSeg(const Point<T>p) const{
-        return fcmp(ori(p), 0.0) == 0 and fcmp((st - p) & (ed - p), 0.0) <= 0;
-    }
-    Real disP2Line(const Point<T> p) const
-    {
-        return Line<double>(projection(p), Point<double>(p)).vec().norm();
+        return sp + vec() * x;
     }
 
-    // notice if you should check Segment intersect or not;
-    // be careful divided by 0
-    Point<Real> getIntersection(Line<Real> l)
-    {
-        Line<Real> h = *this;
-//        return l(((l.st - h.st)^ h.vec()) / (h.vec() ^ l.vec())); // use operator()
-        Real hlst = -h.ori(l.st);
-        Real hled = h.ori(l.ed);
-        return ((l.st * hled) + (l.ed * hlst)) / (hlst + hled);
-
-        // 需要確認+-號的合理性
-        // Area of triangle(l.st, h.st, h.ed) divided by Area of Quadrilateral(h.st, l.st, h.ed, l.ed)
+    bool isSegProperIntersection(Line l) {
+        return l.ori(sp) * l.ori(ep) < 0 and ori(l.sp) * ori(l.ep) < 0;
     }
 
-    Point<Real> projection(const Point<T> p) const
-    {
-        return operator()(((p - st) & vec()) / (Real)(vec().norm()));
+    bool isSegIntersection(Line l){
+        // hsp = 1, hep = 2, lsp = 3, lep = 4
+        double hlsp = ori(l.sp);
+        double hlep = ori(l.ep);
+        double lhsp = l.ori(sp);
+        double lhep = l.ori(ep);
+        if(fcmp(hlsp, 0) == 0 && fcmp(hlep,0) == 0)
+            return isPointOnSeg(l.sp) || isPointOnSeg(l.ep) || l.isPointOnSeg(sp) || l.isPointOnSeg(ep);
+
+        return fcmp(hlsp * hlep) <= 0 && fcmp(lhsp * lhep) <= 0;
     }
-    template <class F>
-    friend ostream &operator<<(ostream &os, const Line<F> &l);
+    
+    bool isPointOnSegProperly(Point p) {
+        return fcmp(ori(p)) == 0 and fcmp(((sp - p) & (ep - p))) < 0;
+    }
+    bool isPointOnSeg(const Point p) {
+        return fcmp(ori(p)) == 0 and fcmp((sp - p) & (ep - p)) <= 0;
+    }
+    double distance(Point p) {
+        return Line(projection(p), p).vec().norm();
+    }
+
+    // notice you should check Segment intersect or not;
+    // be careful divided by 0, like l entirely on Line
+    Point getIntersection(Line l){
+        double hlsp = -ori(l.sp);
+        double hlep = ori(l.ep);
+        return ((l.sp * hlep) + (l.ep * hlsp)) / (hlsp + hlep);
+    }
+
+    Point projection(Point p) 
+    {
+        return operator()(((p - sp) & vec()) / vec().norm());
+    }
 };
-template <class F>
-ostream &operator<<(ostream &os, const Line<F> &l)
+
+// sort by radian, the left is smaller for parallel lines
+auto lcmp = [](Line A, Line B) 
 {
-    return os << "(" << l.st.x << ", " << l.st.y << ") to (" << l.ed.x << ", " << l.ed.y << ")";
-}
+    Point a = A.vec(), b = B.vec();
+    auto sgn = [](Point t) { return (t.y == 0 ? t.x : t.y) < 0; }; // 0 for in [0, pi), 1 for [pi, 2*pi).
+    if (sgn(a) != sgn(b)) // in different side
+        return sgn(a) < sgn(b);
+    else if (abs(a ^ b) == 0) // same 
+        return B.ori(A.sp) > 0;
+    return (a ^ b) > 0;
+};
