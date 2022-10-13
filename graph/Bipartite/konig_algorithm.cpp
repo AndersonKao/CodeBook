adfe6313bc608669e4ee779278fcc0206e657a6d
TunchinKao <tunchinkao@gmail.com>
2022-10-13 14:22:12 +0800

    renew Konig

--- graph/Bipartite/konig_algorithm.cpp
+++ graph/Bipartite/konig_algorithm.cpp
@@ -1,17 +1,23 @@
-#include <vector>
-#include <cstring>
-using namespace std;
-
-// V times DFS O(EV)
-vector<int> V[205];
-// V[i]記錄了左半邊可以配到右邊的那些點
-int match[205]; // A<=B
+const int maxn = 250;
+// time complexity: O(EV), V times DFS
+// G[i]記錄了左半邊可以配到右邊的那些點
+/* bipartite graph be like..
+0\ /-0
+1-X--1
+2/ \/2
+3  /\3
+4 /  4
+5/   5
+.    .
+.    .
+*/
 // match[i] 記錄了右半邊配對到左半邊的哪個點
-bool used[205];
-int n;
+vec<int> G[maxn];
+int match[maxn]; // A <=B
+bool used[maxn];
 bool dfs(int v)
 {
-    for(int e:V[v])
+    for(int e:G[v])
     {
         if( used[e] ) continue;
         used[e] = true;
@@ -23,13 +29,13 @@ bool dfs(int v)
     }
     return false;
 }
-int konig()
+int konig(int n) // num of vertices of left side
 {
     memset(match,-1,sizeof(match));
 
     int ans=0;
     
-    for(int i=1;i<=n;++i)
+    for(int i=0;i<n;++i)
     {
         memset(used, 0, sizeof(used));
         if( dfs(i) )
@@ -37,4 +43,7 @@ int konig()
     }
 
     return ans;
+}
+void addedge(int u, int v){ // left side, right side
+    G[u].eb(v);
 }
