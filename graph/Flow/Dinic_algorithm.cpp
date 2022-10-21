8cb9b1550bccb5bb3d6bf12025b4e9d73c1b1463
TunchinKao <tunchinkao@gmail.com>
2022-10-21 13:03:57 +0800

    swap u, v

--- graph/Flow/Dinic_algorithm.cpp
+++ graph/Flow/Dinic_algorithm.cpp
@@ -3,9 +3,9 @@
 // solving bipartite matching O(E sqrt(V)) better than konig and flow(EV)
 
 struct FlowEdge {
-    int v, u;
+    int u, v;
     long long cap, flow = 0;
-    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+    FlowEdge(int u, int v, long long cap) : u(u), v(v), cap(cap) {}
 };
 
 struct Dinic {
@@ -23,39 +23,39 @@ struct Dinic {
         ptr.resize(n);
     }
 
-    void add_edge(int v, int u, long long cap) {
-        edges.emplace_back(v, u, cap);
-        edges.emplace_back(u, v, 0);
-        adj[v].push_back(m);
-        adj[u].push_back(m + 1);
+    void add_edge(int u, int v, long long cap) {
+        edges.emplace_back(u, v, cap);
+        edges.emplace_back(v, u, 0);
+        adj[u].push_back(m);
+        adj[v].push_back(m + 1);
         m += 2;
     }
 
     bool bfs() {
         while (!q.empty()) {
-            int v = q.front();
+            int u = q.front();
             q.pop();
-            for (int id : adj[v]) {
+            for (int id : adj[u]) {
                 if (edges[id].cap - edges[id].flow < 1) continue;
-                if (level[edges[id].u] != -1) continue;
-                level[edges[id].u] = level[v] + 1;
-                q.push(edges[id].u);
+                if (level[edges[id].v] != -1) continue;
+                level[edges[id].v] = level[u] + 1;
+                q.push(edges[id].v);
             }
         }
         return level[t] != -1;
     }
 
-    long long dfs(int v, long long pushed) {
+    long long dfs(int u, long long pushed) {
         if (pushed == 0) return 0;
-        if (v == t) return pushed;
+        if (u == t) return pushed;
         
-        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
-            int id = adj[v][cid];
-            int u = edges[id].u;
-            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+        for (int& cid = ptr[u]; cid < (int)adj[u].size(); cid++) {
+            int id = adj[u][cid];
+            int v = edges[id].v;
+            if (level[u] + 1 != level[v] || edges[id].cap - edges[id].flow < 1)
                 continue;
                 
-            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            long long tr = dfs(v, min(pushed, edges[id].cap - edges[id].flow));
             
             if (tr == 0) continue;
             edges[id].flow += tr;
@@ -63,7 +63,7 @@ struct Dinic {
             return tr;
         }
         
-        level[v] = -1;
+        level[u] = -1;
         return 0;
     }
 
