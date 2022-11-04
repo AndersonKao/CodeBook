419096a9be4e1d7189d4c2f741455fe7da562793
TunchinKao <tunchinkao@gmail.com>
2022-11-05 00:35:21 +0800

    added NTT

--- number_theory/FFT.cpp
+++ number_theory/FFT.cpp
@@ -87,3 +87,60 @@ int main(){
 	    putchar('0' + res[i]);
     putchar('\n') return 0;
 }
+
+
+// NTT (Number theoretic transform)
+
+const int mod = 7340033;
+const int root = 5;
+const int root_1 = 4404020;
+const int root_pw = 1 << 20;
+
+int inverse(int a, int m){ // returns a^-1 mod m, 0 if not found
+    int x, y;
+    int g = extended_euclidean(a, m, x, y);
+    if (g != 1) {
+        return 0;
+    }
+    else {
+        x = (x % m + m) % m;
+        return x;
+    }
+
+}
+
+void fft(vector<int> & a, bool invert) {
+    int n = a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+
+        if (i < j)
+            swap(a[i], a[j]);
+    }
+
+    for (int len = 2; len <= n; len <<= 1) {
+        int wlen = invert ? root_1 : root;
+        for (int i = len; i < root_pw; i <<= 1)
+            wlen = (int)(1LL * wlen * wlen % mod);
+
+        for (int i = 0; i < n; i += len) {
+            int w = 1;
+            for (int j = 0; j < len / 2; j++) {
+                int u = a[i+j], v = (int)(1LL * a[i+j+len/2] * w % mod);
+                a[i+j] = u + v < mod ? u + v : u + v - mod;
+                a[i+j+len/2] = u - v >= 0 ? u - v : u - v + mod;
+                w = (int)(1LL * w * wlen % mod);
+            }
+        }
+    }
+
+    if (invert) {
+        int n_1 = inverse(n, mod);
+        for (int & x : a)
+            x = (int)(1LL * x * n_1 % mod);
+    }
+}
