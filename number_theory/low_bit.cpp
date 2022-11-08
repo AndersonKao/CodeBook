713c53be3aa85e020088a03cd2059e83eb6173bb
TunchinKao <tunchinkao@gmail.com>
2022-11-08 15:33:21 +0800

    bitmask subset

--- number_theory/low_bit.cpp
+++ number_theory/low_bit.cpp
@@ -1,1 +1,7 @@
 int lowbit(int x) { return x & (~x + 1); }
+// O(3^n) // bitmask dp
+for (int i = 0; i < n; i++) {
+    for (int res = i; res; res = (res - 1) & i) { // all subset
+        
+    }
+} 
