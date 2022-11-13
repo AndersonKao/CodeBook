c0433f46bbe2eff9b7b6c56c238d610e688f3891
liuching <benjack30518@gmail.com>
2022-11-14 01:33:41 +0800

    edit order, add 3_tips

---
+++ VIM/3_tips.cpp
@@ -0,0 +1,6 @@
+Segment Tree, DP, bitwise DP, 枚舉, 枚舉+剪枝, Disjoint Set
+Priority Queue, 單調隊列, Prefix Sum, 偏序
+SCC, AP, Bridge, LCA, 2-SAT
+Flow, Min-cost Max-flow, Bipartite
+Primal test, PollardRho, KMP, Rabin Fingerprint, FFT
+Convex Hull, 旋轉卡尺, 極角排序
