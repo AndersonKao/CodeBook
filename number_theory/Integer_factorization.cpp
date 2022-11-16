6840c3aeed6c46c5e47989d0c4ae3b02e6b60f37
TunchinKao <tunchinkao@gmail.com>
2022-11-16 09:37:27 +0800

    add MOON factorization

--- number_theory/Integer_factorization.cpp
+++ number_theory/Integer_factorization.cpp
@@ -17,3 +17,27 @@ vec<ll> trial_division4(ll n) {
 
     return fac;
 }
+
+// MOON
+vector<int> primes;
+vector<int> LPFs(n + 1, 1);
+for (int i = 2; i < n; ++i) {
+  if (LPFs[i] == 1) {
+    LPFs[i] = i;
+    primes.emplace_back(i);
+  }
+  for (auto p : primes) {
+    if (1LL * i * p > n) break;
+    LPFs[i * p] = i;
+    if (i % p == 0) break;
+  }
+}
+
+void print_prime_factor(int x) {
+  while (x != 1) {
+    int factor = SPFs[x], cnt = 0;
+    for (; x % factor == 0; ++cnt)
+      x /= factor;
+    cout << factor << ": " << cnt << endl;
+  }
+}
