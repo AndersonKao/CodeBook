fdee09b7388c945a2516a44a244de02b95a01482
TunchinKao <tunchinkao@gmail.com>
2022-11-18 15:28:35 +0800

    add slope optimized and Li Chao

--- data_structure/Li_Chao_Tree.cpp
+++ data_structure/Li_Chao_Tree.cpp
@@ -1,52 +1,79 @@
 // Miminimum Li Chao Tree
-typedef long long ftype;
-typedef complex<ftype> point;
-#define x real
-#define y imag
-
-ftype dot(point a, point b) {
-    return (conj(a) * b).x();
-}
-
-ftype f(point a,  ftype x) {
-    return dot(a, {x, 1});
-}
-
-const int maxn = 2e5;
-
-point line[4 * maxn];
-
+const ll inf = 1e18;
+const int maxx = 1e6;
+struct line
+{
+    ll m, b;
+    line() { m = b = inf; }
+    line(ll mm, ll bb){m = mm, b = bb;}
+    ll y(ll x){
+        if(m == inf || b == inf)
+            return inf;
+        return m * x + b;
+    }
+};
+line segs[4 * maxx];
 /*
 a line is  y = k * x + b, using point to represent it.
-y = (k, b) * (x, 1) (dot operation).
 */
-// y = nw.real() * x + nw.imag().
-void add_line(point nw, int idx = 1, int l = 0, int r = maxn)
+void add_line(line nw, int idx = 1, int l = 0, int r = maxx)
 {
-    int m = (l + r) / 2;
-    bool lef = f(nw, l) < f(line[idx], l);
-    bool mid = f(nw, m) < f(line[idx], m);
+    int m = (l + r) >> 1;
+    bool lef = nw.y(l) < segs[idx].y(l);
+    bool mid = nw.y(m) < segs[idx].y(m);
     if(mid) {
         swap(line[idx], nw);
     }
-    if(r - l == 1) {
+    if(l == r) {
         return;
-    } else if(lef != mid) {
+    } else if(lef != mid){
         add_line(nw, 2 * idx, l, m);
     } else {
-        add_line(nw, 2 * idx + 1, m, r);
+        add_line(nw, 2 * idx + 1, m + 1, r);
     }
 }
 
-// get minimum in some point x;
-ftype get(int x, int idx = 1, int l = 0, int r = maxn)
+// get minimum in some line x;
+ll query(int x, int idx = 1, int l = 0, int r = maxx)
 {
     int m = (l + r) / 2;
-    if(r - l == 1) {
-        return f(line[idx], x);
+    if(l == r) {
+        return line[idx].y(x);
     } else if(x < m) {
-        return min(f(line[idx], x), get(x, 2 * idx, l, m));
+        return min(line[idx].y(x), get(x, 2 * idx, l, m));
     } else {
-        return min(f(line[idx], x), get(x, 2 * idx + 1, m, r));
+        return min(line[idx].y(x), get(x, 2 * idx + 1, m + 1, r));
     }
+}
+
+// if the line slope is non-increasing. and query(x) is increasing.
+// dp[i] = min{s[i] * f[j] + dp[j], for all j < i}. f[i] is non-increasing. s[i] increasing. 
+ll inters(line l1,line l2){
+	return (l2.b-l1.b)/(l1.m-l2.m);
+}
+bool check(line l1,line l2,line l3){
+	ll l12=inters(l1,l2);
+	ll l23=inters(l2,l3);
+	return l23<l12;
+}
+ll s[maxn], f[maxn], dp[maxn];
+int main(){
+	int n,k; cin >> n >> k;
+	for(int i=1;i<=n;i++) cin >> s[i];
+	for(int i=1;i<=n;i++) cin >> f[i];
+	
+	deque<line> q;
+	q.push_front({k,0});
+	
+	for(int i=1;i<=n;i++){
+		int x=s[i];
+		while(q.size()>=2 && q[0].y(x)>q[1].y(x)) q.pop_front();
+		dp[i]=q.front().y(x);
+		line now={f[i],dp[i]};
+		
+		if(q.back().a==now.a) continue;
+		while(q.size()>=2 && check(q[q.size()-2],q[q.size()-1],now)) q.pop_back();
+		q.push_back(now);
+	}
+	cout << dp[n] << '\n';
 }
