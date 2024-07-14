6dfe2381144ec717b398e8710720d9e9def66cbe
TunchinKao <tunchinkao@gmail.com>
2024-07-14 22:10:34 +0800

    Update KMP: set failure function to be actual length

--- string/KMP.cpp
+++ string/KMP.cpp
@@ -1,30 +1,37 @@
+// f[i]: failure function, the length of longest proper prefix of P[0..i] that is also the suffix of P[0..i]
 // T for Text, P for Pattern
 vec<int> KMP(const string& P) {
-    vec<int> f(P.size(), -1);
-    int len = f[0] = -1;
+    vec<int> f(P.size(), 0);
     for (int i = 1; i < P.size(); ++i) {
-        while (len != -1 && P[len + 1] != P[i])
-            len = f[len];
-        if (P[len + 1] == P[i])
+		int len = f[i-1];
+        while (len > 0 && P[len] != P[i])
+            len = f[len-1];
+        if (P[len] == P[i])
             ++len;
         f[i] = len;
     }
     return f;
 }
 
-// find S in T
-vec<int> KMP_match(vec<int> fail, const string& S, const string& T) {
+// find S in T in O(|S| + |T|) time O(|S|) space
+vec<int> KMP_match(vec<int> fofS, const string& S, const string& T) {
     vec<int> res; // start from these points
     int n = S.size();
 
-    int i = -1;
+    int len = 0;
     for (int j = 0; j < T.size(); ++j) {
-        while (i != -1 && T[j] != S[i + 1]) 
-            i = fail[i];
+        while (len > 0 && T[j] != S[len]) {
+            len = fofS[len-1];
+		}
 
-        if (T[j] == S[i + 1]) i++;
-        if (i == n - 1) 
-            res.eb(j - n + 1), i = fail[i];
+        if (T[j] == S[len]){
+			len++;
+		}
+
+        if (len == n) {
+            res.eb(j - len + 1);
+			len = fofS[len - 1];
+		}
     }
     
     return res;
